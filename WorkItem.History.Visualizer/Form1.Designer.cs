9bc1a382172909f40f5868622f524c13207f7f38
MarwanELAdawy <marwan.mahmoud@cis.asu.edu.eg>
2017-12-26 18:22:21 +0200

    Visualization of Wirk item in tfs

---
+++ WorkItem.History.Visualizer/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WorkItem.History.Visualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tbWorkItemId = new System.Windows.Forms.TextBox();
+            this.btnAnalyze = new System.Windows.Forms.Button();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.dgWiHistory = new System.Windows.Forms.DataGridView();
+            this.lstVisualizer = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgWiHistory)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tbWorkItemId);
+            this.splitContainer1.Panel1.Controls.Add(this.btnAnalyze);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(684, 461);
+            this.splitContainer1.SplitterDistance = 47;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // tbWorkItemId
+            // 
+            this.tbWorkItemId.Location = new System.Drawing.Point(22, 13);
+            this.tbWorkItemId.Name = "tbWorkItemId";
+            this.tbWorkItemId.Size = new System.Drawing.Size(370, 20);
+            this.tbWorkItemId.TabIndex = 1;
+            // 
+            // btnAnalyze
+            // 
+            this.btnAnalyze.Location = new System.Drawing.Point(424, 12);
+            this.btnAnalyze.Name = "btnAnalyze";
+            this.btnAnalyze.Size = new System.Drawing.Size(238, 23);
+            this.btnAnalyze.TabIndex = 0;
+            this.btnAnalyze.Text = "Visualize";
+            this.btnAnalyze.UseVisualStyleBackColor = true;
+            this.btnAnalyze.Click += new System.EventHandler(this.btnAnalyze_Click);
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.dgWiHistory);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.lstVisualizer);
+            this.splitContainer2.Size = new System.Drawing.Size(684, 410);
+            this.splitContainer2.SplitterDistance = 205;
+            this.splitContainer2.TabIndex = 3;
+            // 
+            // dgWiHistory
+            // 
+            this.dgWiHistory.AllowUserToAddRows = false;
+            this.dgWiHistory.AllowUserToDeleteRows = false;
+            this.dgWiHistory.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dgWiHistory.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgWiHistory.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgWiHistory.Location = new System.Drawing.Point(0, 0);
+            this.dgWiHistory.Name = "dgWiHistory";
+            this.dgWiHistory.Size = new System.Drawing.Size(684, 205);
+            this.dgWiHistory.TabIndex = 3;
+            // 
+            // lstVisualizer
+            // 
+            this.lstVisualizer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstVisualizer.Location = new System.Drawing.Point(0, 0);
+            this.lstVisualizer.Name = "lstVisualizer";
+            this.lstVisualizer.Size = new System.Drawing.Size(684, 201);
+            this.lstVisualizer.TabIndex = 0;
+            this.lstVisualizer.UseCompatibleStateImageBehavior = false;
+            this.lstVisualizer.SelectedIndexChanged += new System.EventHandler(this.lstVisualizer_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(684, 461);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Work Item History Visualizer";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgWiHistory)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox tbWorkItemId;
+        private System.Windows.Forms.Button btnAnalyze;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.DataGridView dgWiHistory;
+        private System.Windows.Forms.ListView lstVisualizer;
+
+    }
+}
+
---
+++ WorkItem.History.Visualizer/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System.Diagnostics;
+using Microsoft.TeamFoundation.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.TeamFoundation.Server;
+using Microsoft.TeamFoundation.WorkItemTracking.Client;
+
+namespace WorkItem.History.Visualizer
+{
+    public partial class Form1 : Form
+    {
+        // Variables I want to use globally
+        //private static TfsTeamProjectCollection _tfs;
+        //private static ProjectInfo _selectedTeamProject;
+        //private static WorkItemStore _wis;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Connect to TFS Using Team Project Picker
+        //public static void ConnectToTfsUsingTeamProjectPicker()
+        //{
+        //    // The  user is allowed to select only one project
+        //    var tfsPp = new TeamProjectPicker(TeamProjectPickerMode.SingleProject, false);
+
+        //    tfsPp.ShowDialog();
+
+        //    // The TFS project collection
+        //    _tfs = tfsPp.SelectedTeamProjectCollection;
+
+        //    if (tfsPp.SelectedProjects.Any())
+        //    {
+        //        //  The selected Team Project
+        //        _selectedTeamProject = tfsPp.SelectedProjects[0];
+        //    }
+        //}
+
+        //public Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItem GetWorkItemDetails(int id)
+        //{
+        //    _wis = _tfs.GetService<WorkItemStore>();
+
+        //    return _wis.GetWorkItem(id);
+        //}
+
+        /// <summary>
+        /// This method takes a work item id, generates a datatable with all revision fields.
+        /// </summary>
+        //public void GetWorkItemHistory(Microsoft.TeamFoundation.WorkItemTracking.Client.WorkItem wi)
+        //{
+        //    var dataTable = new DataTable();
+
+        //    //foreach (Field field in wi.Fields)
+        //    //{
+        //    //    dataTable.Columns.Add(field.Name);
+        //    //}
+
+        //    //foreach (Revision revision in wi.Revisions)
+        //    //{
+        //    //    var row = dataTable.NewRow();
+        //    //    foreach (Field field in wi.Fields)
+        //    //    {
+        //    //        row[field.Name] = revision.Fields[field.Name].Value;
+        //    //    }
+        //    //    dataTable.Rows.Add(row);
+        //    //}
+
+        //    dgWiHistory.DataSource = dataTable;
+        //    dgWiHistory.AutoGenerateColumns = true;
+
+        //    // List of fields to ignore in comparison
+        //    var visualize = new List<string>()
+        //        {
+        //            "Title",
+        //            "State",
+        //            "Rev",
+        //            "Reason",
+        //            "Iteration Path",
+        //            "Assigned To",
+        //            "Effort",
+        //            "Area Path"
+        //        };
+
+        //    // Debug.Write(String.Format("Work Item: {0}{1}", wi.Id, Environment.NewLine));
+        //    lstVisualizer.Items.Add(String.Format("Work Item: {0}{1}", wi.Id, Environment.NewLine));
+
+        //    // Compare Two Work Item Revisions 
+        //    for (var i = 0; i < dgWiHistory.RowCount; i++)
+        //    {
+        //        var currentRow = dgWiHistory.Rows[i];
+
+        //        if (i + 1 < dgWiHistory.RowCount)
+        //        {
+        //            var currentRowPlus1 = dgWiHistory.Rows[i + 1];
+
+        //            // Debug.Write(String.Format("Comparing Revision {0} to {1} {2}", i, i + 1, Environment.NewLine));
+        //            lstVisualizer.Items.Add(String.Format("Comparing Revision {0} to {1} {2}", i, i + 1,
+        //                                                  Environment.NewLine));
+
+        //            bool title = false;
+
+        //            for (int j = 0; j < currentRow.Cells.Count; j++)
+        //            {
+        //                if (!title)
+        //                {
+        //                    //Debug.Write(
+        //                    //    String.Format(String.Format("Changed By '{0}' On '{1}'{2}", currentRow.Cells["Changed By"].Value,
+        //                    //                                currentRow.Cells["Changed Date"].Value, Environment.NewLine)));
+
+        //                    lstVisualizer.Items.Add(
+        //                        String.Format(String.Format("Changed By '{0}' On '{1}'{2}",
+        //                                                    currentRow.Cells["Changed By"].Value,
+        //                                                    currentRow.Cells["Changed Date"].Value, Environment.NewLine)));
+        //                    title = true;
+        //                }
+
+        //                if (visualize.Contains(dataTable.Columns[j].ColumnName))
+        //                {
+        //                    if (currentRow.Cells[j].Value.ToString() != currentRowPlus1.Cells[j].Value.ToString())
+        //                    {
+        //                        //Debug.Write(String.Format("[{0}]: '{1}' => '{2}' {3}", dataTable.Columns[j].ColumnName,
+        //                        //                          currentRow.Cells[j].Value, currentRowPlus1.Cells[j].Value,
+        //                        //                          Environment.NewLine));
+        //                        lstVisualizer.Items.Add(String.Format("[{0}]: '{1}' => '{2}' {3}",
+        //                                                              dataTable.Columns[j].ColumnName,
+        //                                                              currentRow.Cells[j].Value,
+        //                                                              currentRowPlus1.Cells[j].Value,
+        //                                                              Environment.NewLine));
+        //                    }
+        //                }
+        //            }
+        //        }
+        //    }
+        //}
+
+        private void btnAnalyze_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Connect to TFS
+                ConnectToTfsUsingTeamProjectPicker();
+
+                // Enter a valid work item Id
+                if (string.IsNullOrEmpty(tbWorkItemId.Text))
+                {
+                    throw new Exception("Enter a valid Work Item Id");
+                }
+
+                // Get the history
+               // GetWorkItemHistory(GetWorkItemDetails(Convert.ToInt32(tbWorkItemId.Text.Trim())));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(@"Work Item Visualizer", ex.Message, MessageBoxButtons.OK);
+            }
+        }
+
+        private void lstVisualizer_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
